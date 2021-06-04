c09b165320ccc336b27fe0cf48bb990c68fd0215
nebobyeoli <nikkilover17@naver.com>
2021-06-04 19:35:45 +0900

    do a

--- something.c
+++ something.c
@@ -4,9 +4,12 @@ int main(void)
 {
     // I did something in main!
     // Try merging from github on-the-site?
-    
+
     // do something now!
     printf("This is a something\n");
     printf("Hello World!");
+
+    int do_a = 'a';
+    
     return 0;
 }
